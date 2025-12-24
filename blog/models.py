f595cd720dfa2abb3311cd7d305000cf64a2be27
kumarhasmukh697 <kumarhasmukh697@gmail.com>
2025-12-24 13:27:22 +0530

    updated Post model by adding image field

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,10 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='posts')
+    image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+    
+    
