fd7e9ffa575cd0548798ca2285e7f1c4402a7836
kumarhasmukh697 <kumarhasmukh697@gmail.com>
2026-03-20 16:42:59 +0530

    config made for postgres databse to use in render

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -79,6 +79,11 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+DATABASES = {
+  "default": dj_database_url.config(default=f"sqlite:///{BASE_DIR/'db.sqlite3'}", conn_max_age=600, ssl_require=True)
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
