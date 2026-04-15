6e4de3168542f1d4ca361ff2545ced5dc61a12af
kumarhasmukh697 <kumarhasmukh697@gmail.com>
2026-04-15 12:51:30 +0530

    solving image not displying issue for blogs

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -26,4 +26,7 @@ urlpatterns = [
 ]
 
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
